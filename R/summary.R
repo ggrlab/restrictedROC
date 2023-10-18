9ac9da1852bfc2275de0a50d74df2f221ee50dad
gunthergl <gunthergl@gmx.net>
2023-10-18 18:27:39 +0200

    add summary.rROC

---
+++ R/summary.R
@@ -0,0 +1,46 @@
+#' @title Give a summary of a rROC() result
+#' @description \code{\link{rROC}} returns a nested list. It looks something like:
+#' \code{single_restriction <- reslist[[dependent_variables]][[independent_variables]]}
+#'  where every single restriction has done permutations, a p-value and additional info.
+#'  This function gives a summary of the result.
+#' @param object A \code{\link{rROC}} result
+#' @param relevant_cols_regex A regex to filter the columns of a single restriction.
+#' @param current_level The current level of the nested list. This is used internally.
+#'
+#' @export
+#' @return A summary of the result:
+summary.rROC <- function(object,
+                         relevant_cols_regex = c(
+                             "pval.twoside.*", "n_permutations",
+                             "positive_label", ".*\\.auc$", "part"
+                         ),
+                         current_level = 0) {
+    if (all(is.null(object)) || all(is.na(object))) {
+        return(NULL)
+    } else if (!"permutation_pval" %in% names(object)) {
+        current_level <- current_level + 1
+        l_results <- lapply(
+            object,
+            summary.rROC,
+            current_level = current_level,
+            relevant_cols_regex = relevant_cols_regex
+        )
+        return(dplyr::bind_rows(l_results, .id = paste0("level_", current_level)))
+    } else {
+        single_summary <- tibble::as_tibble(data.frame(c(
+            object[["permutation_pval"]],
+            "positive_label" = as.character(object[["positive_label"]]),
+            object["max_total"],
+            object["global"]
+        )))
+        keeping_cols <- c()
+        if (!all(is.na(relevant_cols_regex))) {
+            for (regex_x in c(relevant_cols_regex, "^level_")) {
+                keeping_cols <- c(keeping_cols, grep(regex_x, colnames(single_summary)))
+            }
+            return(single_summary[, keeping_cols])
+        } else {
+            return(single_summary)
+        }
+    }
+}
---
+++ tests/testthat/test-rROC_summary.R
@@ -0,0 +1,31 @@
+devtools::load_all()
+test_that("rROC summary", {
+    # library(restrictedROC)
+    data("aSAH", package = "pROC")
+    set.seed(100)
+    res_df <- rROC(
+        aSAH,
+        dependent_vars = "outcome",
+        independent_vars = "ndka",
+        n_permutations = 2
+    )
+    testthat::expect_equal(dim(summary(res_df)), c(1, 10))
+
+
+    res_df <- rROC(
+        aSAH,
+        dependent_vars = "outcome",
+        independent_vars = c("ndka", "s100b"),
+        n_permutations = 2
+    )
+    testthat::expect_equal(dim(summary(res_df)), c(2, 10))
+
+
+    res_df <- rROC(
+        aSAH,
+        dependent_vars = c("outcome", "gender"),
+        independent_vars = c("ndka", "s100b"),
+        n_permutations = 2
+    )
+    testthat::expect_equal(dim(summary(res_df)), c(4, 10))
+})
